420e90a0d07ca7bd34ae1faf975bf4065fdedf34
Aravind Naveen <38346034+aravindmyd@users.noreply.github.com>
2022-01-06 17:04:18 +0530

    Add files via upload

---
+++ test_repo/airflow_dags/final_dag.py
@@ -0,0 +1,58 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.google.cloud.operators.dataproc import  DataprocCreateClusterOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
+
+default_args = {
+    'depends_on_past': False   
+}
+CLUSTER_NAME = 'test-cluster'
+REGION='us-central1'
+PROJECT_ID='bionic-spot-337303'
+PYSPARK_URI='gs://demo_scripts/scripts/main.py'   
+
+CLUSTER_CONFIG = {
+    "master_config": {
+        "num_instances": 1,
+        "machine_type_uri": "n1-standard-2",
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 500,"num_local_ssds":0},
+    },
+    "software_config":{
+    "image_version":"2.0-debian10",
+    "properties":{"dataproc:dataproc.allow.zero.workers": "true"}
+   
+} ,"initialization_actions":[{
+  "executable_file": "gs://us-central1-airflow-9a7d7fdd-bucket/dags/install.sh"
+}]
+}
+
+PYSPARK_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {"main_python_file_uri": PYSPARK_URI},
+}
+
+with DAG(
+    'Kedro-POC',
+    default_args=default_args,
+    description='A simple DAG to create a Dataproc workflow',
+    schedule_interval=None,
+    start_date = days_ago(2)
+) as dag:
+
+    create_cluster = DataprocCreateClusterOperator(
+        task_id="create_cluster",
+        project_id=PROJECT_ID,
+        cluster_config=CLUSTER_CONFIG,
+        region=REGION,
+        cluster_name=CLUSTER_NAME,
+    )
+
+    submit_job = DataprocSubmitJobOperator(
+        task_id="pyspark_task", 
+        job=PYSPARK_JOB, 
+        location=REGION, 
+        project_id=PROJECT_ID
+    )
+    
+    create_cluster >> submit_job
